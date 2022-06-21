5c69f5945e323b3698ea2f0e7b4f45719564501e
Ryan Prendergast <rprendergast1121@gmail.com>
2022-06-21 00:09:22 -0500

    Updated routes templates

--- routes/sketches.js
+++ routes/sketches.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/', function(req, res, next) {
+  
+});
+
 router.get('/:sketchid', function(req, res, next) {
   res.render('sketch', { id: req.params.sketchid });
 });
