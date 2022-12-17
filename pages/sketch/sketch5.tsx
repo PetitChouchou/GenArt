8eadc1b4ec9de91c06b47c7195c5218207fe8f18
Ryan Prendergast <rprendergast1121@gmail.com>
2022-12-16 22:47:07 -0500

    Sketch 5

---
+++ pages/sketch/sketch5.tsx
@@ -0,0 +1,55 @@
+import p5Types from "p5";
+import SketchPage from "../../components/SketchPage";
+
+export default function Sketch5() {
+  return (
+    <main>
+      <SketchPage title="sketch5" setup={setup} draw={draw} />
+    </main>
+  )
+}
+
+
+/**
+* Sketch 5: Texture
+* My intention with this was to make it like grass, but what I ended up with was something
+* like an audio stream. Might look good as the background to a podcast.
+*
+**/
+const setup = (p5: p5Types, canvasParentRef: Element) => {
+  p5.createCanvas(p5.displayWidth, p5.displayHeight).parent(canvasParentRef);
+}
+
+function draw(p5:p5Types) {
+
+  if (p5.frameCount > 1) {
+    return;
+  }
+  p5.background(249,248,244);
+
+  // Let's add a grass like texture with a bunch of vertical lines.
+  // What do we want to vary exactly? Do we want to have a set start point and vary
+  // the up and down direction, or would that look too symmetric? Let's see.
+
+  const NUM_COLS = 400;
+  const NUM_ROWS = 20;
+
+
+  for (let i = 0; i < NUM_COLS; i++) {
+    for (let j = 0; j < NUM_ROWS; j++ ) {
+      const length = p5.randomGaussian(20,15);
+      const alpha = p5.randomGaussian(100, 15);
+      p5.stroke(31,26,14,alpha);
+
+
+      const top_coord = p5.displayHeight * (j / NUM_ROWS) - (length / 2);
+      const bottom = p5.displayHeight * (j / NUM_ROWS) + (length / 2);
+
+      const x = p5.displayWidth * (i / NUM_COLS);
+
+      console.log(x + "," + bottom + "," + top_coord);
+      p5.line(x, bottom, x, top_coord);
+
+    }
+  }
+}
