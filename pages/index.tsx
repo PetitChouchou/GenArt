2ee9168776c2b797d6177d5909865a73760fb45d
Ryan Prendergast <rprendergast1121@gmail.com>
2022-12-17 02:04:44 -0500

    Not nonexistant styling on index

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,19 +24,21 @@ export default function Home(props: HomeProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <>
-          <div>Welcome to my generative sketches</div>
+      <main className="bg-slate-50 h-screen flex flex-row text-stone-900 place-content-center">
+        <div className="w-3/4 md:w2/3 lg:w-1/2 text-lg">
+          <h1 className="pt-16 pb-1 text-4xl font-bold">~ Generative</h1>
+          <h3 className="font-medium italic text-xl pb-6">sketches in <a href='https://p5js.org/' target="_blank" className='hover:text-stone-500' rel="noreferrer">p5.js</a></h3>
           { props.sketches.map((sketch) => {
             return (
-              <div key={sketch.slug}>
-                <Link href={`/sketch/${sketch.slug}`}>
-                  {sketch.slug}
-                </Link>
-              </div>
+              <p key={sketch.slug} className="text-slate-500 mt-3">
+                <Link
+                href={`/sketch/${sketch.slug}`}
+                className="hover:text-slate-800"
+                >{sketch.slug}</Link>
+              </p>
             )
           })}
-        </>
+        </div>
       </main>
     </>
   )
