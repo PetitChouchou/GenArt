0f681c0f9bcb1d332ae9c2cb3d26ba0635159154
Raph <Raphlhaddad29@gmail.com>
2024-02-17 16:59:57 -0500

    Update

---
+++ pages/sketch/raph.tsx
@@ -0,0 +1,26 @@
+import p5Types from "p5";
+import SketchPage from "../../components/SketchPage";
+
+export default function Sketch1 () {
+  return (
+  <main>
+    <div>raph</div>
+    <SketchPage title="raphhhhhhh" setup={setup} draw={draw} />
+  </main>
+  )
+}
+
+// A demo that creates a canvas and follows the demo on https://p5js.org/get-started/.
+const setup = (p5: p5Types, canvasParentRef: Element) => {
+  p5.createCanvas(p5.windowWidth, p5.windowHeight).parent(canvasParentRef);
+  p5.background(56)
+}
+
+const draw = (p5: p5Types) => {
+  if (p5.mouseIsPressed) {
+    p5.fill(0);
+  } else {
+    p5.fill(255);
+  }
+  p5.ellipse(p5.mouseX, p5.mouseY, 80, 80)
+}
